d22555455835964679dccbe54e6745141e413aa5
9246692a <abdulloxislomov39@gmail.com>
2023-03-03 21:43:06 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,47 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+# class Person:
+#     def __init__(self, ismi, yoshi, jinsi):
+#         self.ismi = ismi
+#         self.yoshi = yoshi
+#         self.jinsi = jinsi
+#
+# Jek = Person('Jek', '23', 'erkak')
+# Juli = Person('Juli', '18', 'ayol')
+# print(f'Mening ismim {Jek.ismi}, yoshim {Jek.yoshi}da, jinsim {Jek.jinsi}')
+
+
+# class Car:
+#     def __init__(self, color, probeg):
+#         self.color = color
+#         self.probek = probeg
+#
+#     def __str__(self):
+#         return f'My cocar lor {self.color}, probek {self.probeg}'
+#
+#
+# blue = Car('blue', '30_000')
+# red = Car('red', '20_000')
+# # print(blue)
+# # print(red)
+#     def drive(self )
+#
+# class Car:
+#  for Car in ('blue', 'red'):
+#     print(Car)
